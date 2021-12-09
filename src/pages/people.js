dd4617156926868d0d47afd49b319904ccd93335
xuuuluuu <xu_lu@hotmail.com>
2021-12-09 21:51:39 +0800

    update

--- src/pages/people.js
+++ src/pages/people.js
@@ -66,24 +66,24 @@ export default () => (
         }
       )}
     </div>
-    <h1>Alumni</h1>
-    <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-      { Object.keys(peopleData).map((name, i) => {
-          if (peopleData[name]["role"] === "alumni") {
-            return (
-              <PeopleCard
-                key={i}
-                name={name}
-                title={peopleData[name]["title"]}
-                researchInterest={peopleData[name]["researchInterest"]}
-                googlescholar={peopleData[name]["googlescholar"]}
-                github={peopleData[name]["github"]}
-                linkedin={peopleData[name]["linkedin"]}
-              />
-            )
-          }
-        }
-      )}
-    </div>
+    {/*<h1>Alumni</h1>*/}
+    {/*<div style={{ display: 'flex', flexWrap: 'wrap' }}>*/}
+    {/*  { Object.keys(peopleData).map((name, i) => {*/}
+    {/*      if (peopleData[name]["role"] === "alumni") {*/}
+    {/*        return (*/}
+    {/*          <PeopleCard*/}
+    {/*            key={i}*/}
+    {/*            name={name}*/}
+    {/*            title={peopleData[name]["title"]}*/}
+    {/*            researchInterest={peopleData[name]["researchInterest"]}*/}
+    {/*            googlescholar={peopleData[name]["googlescholar"]}*/}
+    {/*            github={peopleData[name]["github"]}*/}
+    {/*            linkedin={peopleData[name]["linkedin"]}*/}
+    {/*          />*/}
+    {/*        )*/}
+    {/*      }*/}
+    {/*    }*/}
+    {/*  )}*/}
+    {/*</div>*/}
   </div>
 )
