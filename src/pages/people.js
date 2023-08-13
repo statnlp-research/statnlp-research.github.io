6b0fa9bd6ed3db0443dff0ba3829cfd8fc9f697c
Jin Ziqi <Xalphinions@outlook.com>
2023-08-13 10:06:19 -0400

    ADD: update Xiao Yao, and many others roles.

--- src/pages/people.js
+++ src/pages/people.js
@@ -66,7 +66,27 @@ export default () => (
         }
       )}
     </div>
-    {/*<h1>Alumni</h1>*/}
+    <h1>Alumni</h1>
+    <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+      { Object.keys(peopleData).map((name, i) => {
+          if (peopleData[name]["role"] === "alumni") {
+            console.log(name)
+            return (
+              <PeopleCard
+                key={i}
+                name={name}
+                title={peopleData[name]["title"]}
+                researchInterest={peopleData[name]["researchInterest"]}
+                googlescholar={peopleData[name]["googlescholar"]}
+                github={peopleData[name]["github"]}
+                linkedin={peopleData[name]["linkedin"]}
+              />
+            )
+          }
+        }
+      )}
+    </div>
+    {/* <h1>Alumni</h1>*/}
     {/*<div style={{ display: 'flex', flexWrap: 'wrap' }}>*/}
     {/*  { Object.keys(peopleData).map((name, i) => {*/}
     {/*      if (peopleData[name]["role"] === "alumni") {*/}
@@ -84,6 +104,6 @@ export default () => (
     {/*      }*/}
     {/*    }*/}
     {/*  )}*/}
-    {/*</div>*/}
+    {/*</div> */}
   </div>
 )
